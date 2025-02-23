e79fd024c7b1351dd408053a3b6b423ce9779d25
Sefinek <contact@sefinek.net>
2025-02-23 12:01:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const pullAndUpdateWAFRules = async () => {
 };
 
 // https://crontab.guru
-new CronJob(process.env.RULES_UPDATE_SCHEDULE || '0 11,14,16,18,20 * * *', pullAndUpdateWAFRules, null, true, 'UTC'); // At minute 0 past hour 11, 14, 16, 18, and 20.
+new CronJob(process.env.RULES_UPDATE_SCHEDULE || '0 11,14,16,18,20 * * *', pullAndUpdateWAFRules, null, true, 'UTC'); // At minute 0 past hour 11, 14, 16, 18, and 20
 (async () => pullAndUpdateWAFRules())();
 
 if (process.env.NODE_ENV === 'production') {
 	const restartApp = require('./jobs/reloadApp.js');
-	new CronJob(process.env.APP_RESTART_SCHEDULE || '0 15 * * *', restartApp, null, true, 'UTC'); // At 15:00.
+	new CronJob(process.env.APP_RESTART_SCHEDULE || '0 15 * * *', restartApp, null, true, 'UTC'); // At 15:00
 }
 
 // PM2
