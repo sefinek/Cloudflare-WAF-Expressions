291af25ca022579dfb9d24e470bb677cc1ea286e
Sefinek <contact@sefinek.net>
2025-04-24 16:16:19 +0200

    Improvements (do not restart if there are no updates)

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const { CronJob } = require('cron');
 const simpleGit = require('simple-git');
 const git = simpleGit();
 const executeWAFRuleUpdate = require('./jobs/updateWAFRules.js');
+const restartApp = require('./jobs/reloadApp.js');
 const log = require('./scripts/log.js');
 const { version } = require('./package.json');
 
 log(0, `Cloned version: v${version}`);
 
-const pullAndUpdateWAFRules = async () => {
+const gitPullAndMaybeRestart = async () => {
 	try {
 		const { summary } = await git.pull();
-		log(0, JSON.stringify(summary));
+		if (!(summary.changes > 0 || summary.deletions > 0 || summary.insertions > 0)) return;
 
-		await executeWAFRuleUpdate();
+		log(0, JSON.stringify(summary));
+		await restartApp();
 	} catch (err) {
 		log(3, 'Git pull failed:', err.message);
 	}
-
-	// log(0, '========= New update (30.10.2024). Consider removing all rules and make sure you have the latest version of this script. Then, run it again. =========');
 };
 
-// https://crontab.guru
-new CronJob(process.env.RULES_UPDATE_SCHEDULE || '0 11,14,16,18,20 * * *', pullAndUpdateWAFRules, null, true, 'UTC'); // At minute 0 past hour 11, 14, 16, 18, and 20
-(async () => pullAndUpdateWAFRules())();
+// Cron jobs
+new CronJob(process.env.RULES_UPDATE_CRON || '0 15 */2 * *', gitPullAndMaybeRestart, null, true, 'UTC'); //
+new CronJob(process.env.GIT_PULL_CRON || '0 11,14,16,18,20 * * *', executeWAFRuleUpdate, null, true, 'UTC'); //
 
-if (process.env.NODE_ENV === 'production') {
-	const restartApp = require('./jobs/reloadApp.js');
-	new CronJob(process.env.APP_RESTART_SCHEDULE || '0 15 * * *', restartApp, null, true, 'UTC'); // At 15:00
-}
+// Run the jobs immediately
+(async () => {
+	await gitPullAndMaybeRestart();
+	await executeWAFRuleUpdate();
+})();
 
 // PM2
 process.send && process.send('ready');
