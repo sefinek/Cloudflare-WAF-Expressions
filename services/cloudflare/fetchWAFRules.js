113532986a4f562ae26ff2d6d4965a125678c204
Sefinek <contact@sefinek.net>
2025-08-23 02:23:25 +0200

    Remove unnecessary log statement from WAF rules fetching function

--- services/cloudflare/fetchWAFRules.js
+++ services/cloudflare/fetchWAFRules.js
@@ -2,8 +2,6 @@ const log = require('../../scripts/log.js');
 const { axios } = require('../axios.js');
 
 module.exports = async zoneId => {
-	log('Fetching WAF rules...');
-
 	try {
 		const { data } = await axios.get(`/zones/${zoneId}/firewall/rules`);
 		if (!data.success) throw new Error(`Failed to fetch rules: ${data?.errors}`);
